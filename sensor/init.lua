12aabd9710bb1f0b31015db25781f0bbdd63c343
Frovu <sfrovis@gmail.com>
2021-01-26 12:54:48 +0300

    fix: init.lua

--- sensor/init.lua
+++ sensor/init.lua
@@ -8,12 +8,17 @@ dofile("settings.lua")
 local measure = require("measure")
 local ds18b20 = require("ds18b20")
 
+local DS18B20_PIN = 3
 local LED_PIN = 4
+
+ds18b20.init(DS18B20_PIN)
 
 gpio.mode(LED_PIN, gpio.OUTPUT)
 gpio.write(LED_PIN, gpio.HIGH)
 
 print("\nInit timer rate = "..settings.data_rate.." s")
 tmr.create():alarm(settings.data_rate * 1000, tmr.ALARM_AUTO, function()
-	ds18b20.measure(measure.send)
+	gpio.write(LED_PIN, gpio.LOW)
+	ds18b20.measure(DS18B20_PIN, measure.send)
+	gpio.write(LED_PIN, gpio.HIGH)
 end)
