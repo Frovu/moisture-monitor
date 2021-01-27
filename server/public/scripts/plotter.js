098aed14392293bb23b6dba860823116d3f2ab35
unknown <egorizzee@gmail.com>
2021-01-27 17:49:33 +0300

    very bad 2

--- server/public/scripts/plotter.js
+++ server/public/scripts/plotter.js
@@ -2,27 +2,24 @@ var dates = [];
 var moistures = [];
 var temperatures = [];
 var data = [];
+var graph;
 
 // initial data
 getDevices().then(function (response) {
     let deviceID = Object.keys(response)[0];
     getData(deviceID).then(function (response) {
         let rows = response["rows"];
-
         rows.forEach((row) => dates.push(new Date(row[0]))); 
         rows.forEach((row) => moistures.push(row[1])); 
         rows.forEach((row) => temperatures.push(row[2])); 
-        console.log(dates[0] + " " + temperatures[0]);
-
-        //dates.forEach((date, i) => m.push([date, moistures[i]]));
-        //dates.forEach((date, i) => t.push([date, temperatures[i]]));
+        
         dates.forEach((date, i) => data.push([date, moistures[i], temperatures[i]]));
         plot();
     });
 });
 
 function plot () {
-    new Dygraph(document.getElementById("div_g"), data,
+    graph = new Dygraph(document.getElementById("div_g"), data,
     {
         title: "Moisture Sensor",
         showRoller: true,
@@ -40,51 +37,27 @@ function plot () {
           }
     });
 
-    // It sucks that these things aren't objects, and we need to store state in window.
-    /*
+    // GET "update"?
     window.intervalId = setInterval(function() {
         getDevices().then(function (response) {
             let deviceID = Object.keys(response)[0];
             getData(deviceID).then(function (response) {
-                //plot(response["fields"], response["rows"]);
                 let rows = response["rows"];
-                let newDate = row[rows.length-1];
-                let newMoisture = row[rows.length-1];
-                rows.forEach((row) => dates.push(row[0])); 
+                rows.forEach((row) => dates.push(new Date(row[0]))); 
                 rows.forEach((row) => moistures.push(row[1])); 
                 rows.forEach((row) => temperatures.push(row[2])); 
+
+                dates.forEach((date, i) => data.push([date, moistures[i], temperatures[i]]));
+                graph.updateOptions( { 'file': data } );
             });
-    });
-    var x = new Date();  // current time
-    var y = Math.random();
-    t.push([x, y]);
-    g.updateOptions( { 'file': t } );
-    }, 1000);*/
-}
-
-function update () {
-
+        });
+        console.log("updated");
+    }, 10000);
 }
 
 function ready () {
 
 }
-
-/*
-function plot (fields, rows) {
-    rows.forEach((row) => times.push(row[0])); 
-    rows.forEach((row) => moistures.push(row[1])); 
-    rows.forEach((row) => temperatures.push(row[2])); 
-
-    new Dygraph(div, "ny-vs-sf.txt", {
-        legend: 'always',
-        title: 'NYC vs. SF',
-        showRoller: true,
-        rollPeriod: 14,
-        customBars: true,
-        ylabel: 'Temperature (F)',
-    });
-}*/
 
 async function getDevices () {
     const response = await fetch('api/devices', {
