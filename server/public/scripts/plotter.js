1df8dc3b74abe49fcfa7b9a5d13fc2ff055ceb70
Frovu <sfrovis@gmail.com>
2021-03-11 16:50:11 +0300

    refactor: whole frontend js

--- server/public/scripts/plotter.js
+++ server/public/scripts/plotter.js
@@ -1,95 +1,64 @@
-var dates = [];
-var moistures = [];
-var temperatures = [];
-var data = [];
-var graph;
-
+window.intervals = [];
 // initial data
-getDevices().then(function (response) {
-    let deviceID = Object.keys(response)[0];
-    getData(deviceID).then(function (response) {
-        let rows = response["rows"];
-        rows.forEach((row) => dates.push(new Date(row[0]))); 
-        rows.forEach((row) => moistures.push(row[1])); 
-        rows.forEach((row) => temperatures.push(row[2])); 
-        
-        dates.forEach((date, i) => data.push([date, moistures[i], temperatures[i]]));
-        plot();
-    });
+getDevices().then((response) => {
+	const deviceID = Object.keys(response)[0];
+	plot(deviceID);
 });
 
-function plot () {
-    graph = new Dygraph(document.getElementById("div_g"), data,
-    {
-        title: "Moisture Sensor",
-        legend: 'always',
-        showRoller: true,
-        strokeWidth: 2.0,
-        labels: ['Date', 'Moisture', 'Temperature'],
-        ylabel: 'Moisture Axis',
-        y2label: 'Temperature Axis',
-        series: {
-            'Moisture': {
-                axis: 'y1'
-            },
-            'Temperature': {
-                axis: 'y2'
-            }
-        },
-        axes: {
-            y1: {
-                valueRange: [650, 750],
-                labelsKMB: true
-            }
-          },
-    });
+const FIELD_TO_LABEL = {
+	moisture: 'Moisture',
+	temperature: 'Temperature',
+	date: 'Date'
+};
 
-    // GET "update"?
-    window.intervalId = setInterval(function() {
-        getDevices().then(function (response) {
-            let deviceID = Object.keys(response)[0];
-            getData(deviceID).then(function (response) {
-                let rows = response["rows"];
-                rows.forEach((row) => dates.push(new Date(row[0]))); 
-                rows.forEach((row) => moistures.push(row[1])); 
-                rows.forEach((row) => temperatures.push(row[2])); 
+async function plot(devId) {
+	const data = await getData(devId);
+	const labels = data.fields.map(f => FIELD_TO_LABEL[f]);
+	const graph = new Dygraph(document.getElementById('div_g'), data.rows,
+		{
+			title: 'Moisture Sensor',
+			legend: 'always',
+			showRoller: true,
+			strokeWidth: 2.0,
+			labels: labels,
+			ylabel: 'Moisture Axis',
+			y2label: 'Temperature Axis',
+			series: {
+				'Moisture': {
+					axis: 'y1'
+				},
+				'Temperature': {
+					axis: 'y2'
+				}
+			},
+			axes: {
+				y1: {
+					valueRange: [650, 750],
+					labelsKMB: true
+				}
+			},
+		});
 
-                dates.forEach((date, i) => data.push([date, moistures[i], temperatures[i]]));
-                graph.updateOptions( { 'file': data } );
-            });
-        });
-        console.log("updated");
-    }, 10000);
+	// GET "update"?
+	window.intervals.push(setInterval(() => {
+		getData(devId)
+			.then(data => graph.updateOptions({'file': data.rows}));
+		console.log('updated');
+	}, 10000));
 }
 
-function ready () {
-
+async function getDevices() {
+	const response = await fetch('api/devices');
+	if(response && response.ok)
+		return await response.json();
+	//show_error("Server didn't respond or some error occured");
+	return null;
 }
 
-async function getDevices () {
-    const response = await fetch('api/devices', {
-        method: 'GET',
-        credentials: "same-origin",
-    }).catch((e) => {
-        //show_error(e);
-    });
-    
-    if(response && response.ok)
-        return await response.json();
-    //show_error("Server didn't respond or some error occured");
-    return null;
-}
-
-async function getData () {
-    const response = await fetch('api/data?dev=1', {
-        method: 'GET',
-        credentials: "same-origin",
-    }).catch((e) => {
-        //show_error(e);
-    });
-    
-    if(response && response.ok)
-        return await response.json();
-    //show_error("Server didn't respond or some error occured");
-    return null;
+async function getData(devId) {
+	const response = await fetch(`api/data?dev=${devId}`);
+	if(!response || !response.ok)
+		return null;
+	//show_error("Server didn't respond or some error occured");
+	return await response.json();
 }
